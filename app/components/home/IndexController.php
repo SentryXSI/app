10acbbd617f24ece1be2de28e43749dfe9d9d092
SentryXSI <34962312+SentryXSI@users.noreply.github.com>
2019-10-04 23:55:52 +1000

    add IndexController

---
+++ app/components/home/IndexController.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Components\Home;
+
+use NinjaSentry\Rei\{
+    Http,
+    Kernel\BaseController
+};
+
+/**-----------------------------------------------------------------------------
+ *
+ * Class IndexController
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * @package App\Components\Homes
+ */
+final class IndexController extends BaseController
+{
+    /**
+     * IndexController constructor.
+     *
+     * @param \NinjaSentry\Rei\Http\Response $response
+     */
+    public function __construct( Http\Response $response ){
+        parent::__construct( $response );
+    }
+
+    /**-------------------------------------------------------------------------
+     *
+     * Get Index
+     *
+     * -------------------------------------------------------------------------
+     *
+     * @return mixed|void
+     * @throws \Exception
+     */
+    public function getIndex(){
+        $this->getHtmlDocument();
+    }
+}
